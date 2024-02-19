e5fa1c3ef7b089d92d952c5e3740ee31dec88cd3
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-19 13:58:59 +0300

    add new project

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Ваня Науменко on 19.02.24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
